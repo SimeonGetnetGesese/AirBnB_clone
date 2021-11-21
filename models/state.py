00322cbbbf2ba7065a6e43db13636ec960ed0212
Yishak-KD <yisak04@gmail.com>
2021-11-21 10:50:20 -0800

    State model

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""State class is defined"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """A state class is created that inherits from BaseModel
+        Attributes:
+            name(str): Name of the state.
+    """
+
+
+    name = ""
