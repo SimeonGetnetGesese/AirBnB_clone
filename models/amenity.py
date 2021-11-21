ebf3207185f40fa35505ae8bc5c4a452690ebeee
Yishak-KD <yisak04@gmail.com>
2021-11-21 11:05:41 -0800

    Amenity class

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Amenity class."""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Class amenity.
+        Attribute:
+            name (str): Name of the amenity.
+    """
+
+    name = ""
